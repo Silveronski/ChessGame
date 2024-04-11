1c4b2f1c851e34e519cd1c2c83cbdabd0d10b617
Silveronski <153466946+Silveronski@users.noreply.github.com>
2024-04-11 19:21:41 +0300

    ee

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -9,6 +9,7 @@ export const routes = app => {
     });
 
     app.get('/black', (req, res) => {
+        console.log(req);
         if (!games[req.query.code]) {
             return res.redirect('/?error=invalidCode');
         }
